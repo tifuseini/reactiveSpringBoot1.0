37ae7c5d748fd0580c0ee828f5a129a1084b294c
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-16 19:18:53 +0000

    LoadDatabase Update

--- src/main/java/com/springboot/socialMediaPlatform/utils/LoadDatabase.java
+++ src/main/java/com/springboot/socialMediaPlatform/utils/LoadDatabase.java
@@ -1,4 +1,26 @@
 package com.springboot.socialMediaPlatform.utils;
 
+import com.springboot.socialMediaPlatform.model.Chapter;
+import com.springboot.socialMediaPlatform.repository.ChapterRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+
+@Configuration
 public class LoadDatabase {
+
+    @Bean
+    CommandLineRunner init(ChapterRepository chapterRepository){
+        return args -> {
+            Flux.just(
+                    new Chapter("Chapter 1"),
+                    new Chapter("Chapter 2"),
+                    new Chapter("Chapter 3"))
+                    .flatMap(chapterRepository::save)
+                    .subscribe(System.out::println);
+
+        };
+    }
+
 }
