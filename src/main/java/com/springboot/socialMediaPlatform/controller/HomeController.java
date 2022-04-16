df59403d78243fe38bb744d16c66245f8285a3d1
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-16 17:52:06 +0000

    Home Controller Update

--- src/main/java/com/springboot/socialMediaPlatform/controller/HomeController.java
+++ src/main/java/com/springboot/socialMediaPlatform/controller/HomeController.java
@@ -1,4 +1,15 @@
 package com.springboot.socialMediaPlatform.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping
+    public String greeting(@RequestParam(required = false,defaultValue = "")String name){
+        return name.equals("") ? "Hey!" : "Hey, " + name + "!";
+    }
 }
