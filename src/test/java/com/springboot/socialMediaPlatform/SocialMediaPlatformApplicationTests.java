ea4e63b2450685dc8d37acaf75cb749d3eedf13c
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-16 16:59:54 +0000

    Source Files

---
+++ src/test/java/com/springboot/socialMediaPlatform/SocialMediaPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.socialMediaPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
