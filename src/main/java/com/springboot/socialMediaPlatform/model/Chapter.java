8c4fd671dc55f2a500293960bcba2b2f1df5f8ee
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-16 19:02:33 +0000

    Chapter Class Update

--- src/main/java/com/springboot/socialMediaPlatform/model/Chapter.java
+++ src/main/java/com/springboot/socialMediaPlatform/model/Chapter.java
@@ -1,4 +1,20 @@
 package com.springboot.socialMediaPlatform.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
 public class Chapter {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    public Chapter(String name){
+        this.name = name;
+    }
+
 }
