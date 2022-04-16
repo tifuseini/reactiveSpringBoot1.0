d0c68506fda7174ede2232dcb886cca527985bcd
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-16 19:26:26 +0000

    Chapter Controller Class Updatd

--- src/main/java/com/springboot/socialMediaPlatform/controller/ChapterController.java
+++ src/main/java/com/springboot/socialMediaPlatform/controller/ChapterController.java
@@ -1,4 +1,22 @@
 package com.springboot.socialMediaPlatform.controller;
 
+import com.springboot.socialMediaPlatform.model.Chapter;
+import com.springboot.socialMediaPlatform.repository.ChapterRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
 public class ChapterController {
+
+    private final ChapterRepository chapterRepository;
+
+    public ChapterController(ChapterRepository chapterRepository){
+        this.chapterRepository = chapterRepository;
+    }
+
+    @GetMapping("/chapters")
+    public Flux<Chapter> listing(){
+        return chapterRepository.findAll();
+    }
 }
